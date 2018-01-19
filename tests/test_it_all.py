c66db8e594fbcc28ecd0ff43f942931df121b51d
Chris McComb <ccmcc2012@gmail.com>
2018-01-19 12:16:02 -0500

    Now with Nelson metric.

--- tests/test_it_all.py
+++ tests/test_it_all.py
@@ -10,6 +10,9 @@ class Test(unittest.TestCase):
         temp = TeamVariety.Corpus(pkg_resources.resource_filename('tests', 'designs.csv'),
                                   pkg_resources.resource_filename('tests', 'participants.csv'))
 
+        temp.genealogy_levels = ['PhysicalPrinciple', 'WorkingPrinciple', 'Embodiment']
+        temp.weights = [10, 5, 2, 1]
+
         # Make sure we're only dealing with one level
         temp.remove_participants("Level", 1)
 
@@ -17,6 +20,6 @@ class Test(unittest.TestCase):
         temp.compute_individual_variety()
 
         # Get all conditions
-        v, c = temp.get_all_conditions(100, 4)
+        v, c, t = temp.get_all_conditions(100, 4)
 
         self.assertEqual(True, True)
